cf51d992754a1ac7d3140357b90079b0a36def3c
tiago200494 <tiagosilva-01@hotmail.com>
2022-04-28 11:29:39 -0300

    Alteração repositório funcionário
    alteração repositório funcionário

--- src/main/java/com/facol/hardgamerstore/dados/RepositorioFuncionario.java
+++ src/main/java/com/facol/hardgamerstore/dados/RepositorioFuncionario.java
@@ -2,36 +2,44 @@ package com.facol.hardgamerstore.dados;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
-import com.facol.hardgamerstore.model.Funcionario;
 
-public class RepositorioFuncionario extends RepositorioGenerico<Funcionario>{
+import com.facol.hardgamerstore.modelo.Cliente;
+import com.facol.hardgamerstore.modelo.Funcionario;
 
+public class RepositorioFuncionario {
+
+	@PersistenceContext(name = "hardgamerstore")
+	private EntityManager entityManager;
 	
-	@Override
-	public void salvar(Funcionario funcionario) {
-		this.salvar(funcionario);
+	public void criar(Funcionario funcionario) {
+		this.entityManager.persist(funcionario);
 	}
 	
-	@Override
-	public Funcionario buscar(long id) {
-		return this.buscar(id);
-	}
-	
-	@Override
-	public void excluir(Funcionario funcionario) {
-		this.excluir(funcionario);
-	}
-	
-	@Override
 	public void alterar(Funcionario funcionario) {
-		this.alterar(funcionario);
+		this.entityManager.merge(funcionario);
 	}
 	
-	@Override
+	@SuppressWarnings("unchecked")
 	public List<Funcionario> listar() {
-		return this.listar();
+		List<Funcionario> result = null;
+		Query query = this.entityManager.createQuery("FROM Funcionario entity");
+		result = query.getResultList();
+		
+		return result;
 	}
+
+	public void remover(Funcionario funcionario) {
+		Query query = this.entityManager.createQuery("FROM Funcionario entity WHERE entity.id = :id");
+		query.setParameter("id", funcionario.getId());
+		Funcionario removeFuncionario = (Funcionario)query.getSingleResult();
+		this.entityManager.remove(removeFuncionario);
+	}	
+
+	
 	
 	
 }
